25374303aa2ac5bff5ae3418f02708544049d6da
Dwaipayan <dwaipayan2609@gmail.com>
2019-10-18 11:40:02 +0530

    Added local copy of Graph Model

--- controller/Home.controller.js
+++ controller/Home.controller.js
@@ -2,16 +2,19 @@ sap.ui.define([
 	'./BaseController',
 	'sap/ui/model/json/JSONModel',
 	'sap/ui/Device',
-	'kronos/ui/graphapp/model/formatter'
-], function (BaseController, JSONModel, Device, formatter) {
+	'kronos/ui/graphapp/model/formatter',
+	"sap/ui/model/BindingMode"
+], function (BaseController, JSONModel, Device, formatter, BindingMode) {
 	"use strict";
 	return BaseController.extend("kronos.ui.graphapp.controller.Home", {
 		formatter: formatter,
 
 		onInit: function () {
 			var EventBus = sap.ui.getCore().getEventBus();
+			var graphModel = {};
 			EventBus.subscribe("FilterChannel", "DialogClose", this._handleFilterEvent, this)
-			var graphModel = this.getOwnerComponent().getModel("graph");
+			graphModel = this.getOwnerComponent().getModel("graph");
+			this.parentModel = jQuery.sap.extend(true, {}, graphModel);
 			graphModel.setSizeLimit(Number.MAX_SAFE_INTEGER);
 			this.setModel(graphModel);
 			this.oModelSettings = new JSONModel({
@@ -25,8 +28,36 @@ sap.ui.define([
 			this.oGraph._fZoomLevel = 0.75;
 		},
 
-		_handleFilterEvent: function (sChanel, sEvent, sData) {
+		_filterNearestNeighborhood: function (localModel, filter) {
+			//Remove duplicates from filter.
+			if (filter.length > 0) {
+				filter = Array.from(new Set(filter));
+				debugger;
+				var unfilteredArrLines = localModel.lines;
+				//Use any line going to or from the set of nearest neighborhood filter nodes.
+				var filteredArrLines = unfilteredArrLines.filter((line) => { return filter.includes(line.from) || filter.includes(line.to) });
+				localModel.lines = filteredArrLines;
+				//Remove all nodes other than in filters.
+				var unfilteredArrNodes = localModel.nodes;
+				var filteredArrNodes = unfilteredArrNodes.filter((node) => {
+					return filteredArrLines.some((line) =>
+						line.from == node.key || line.to == node.key)
+				});
+				localModel.nodes = filteredArrNodes;
+			}
+			return localModel;
+		},
 
+		_handleFilterEvent: function (sChanel, sEvent, sData) {
+			//Get all nodes from parent model.
+			//Copy parent model to local model
+			//Apply filters to local Model.
+			debugger;
+			var localModel = jQuery.sap.extend(true, {}, this.parentModel.oData);
+			localModel = this._filterNearestNeighborhood(localModel, sData.nearestNeighborhood);
+			this.byId("graph").getModel().setProperty("/nodes", localModel.nodes);
+			this.byId("graph").getModel().setProperty("/lines", localModel.lines);
+			this.byId("graph").getModel().setProperty("/groups", localModel.groups);
 		}
 	});
 });
